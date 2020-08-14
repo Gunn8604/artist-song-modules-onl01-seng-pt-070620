1a93504d4c9eecd5ac37d4c783256531472bb1da
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-08-14 01:00:47 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,16 +1,16 @@
 module Memorable
-
-  def reset_all
-    self.all.clear
+  module ClassMethods
+    def reset_all
+      all.clear
+    end
+  
+    def count
+      all.count
+    end
   end
   
-  def count
-    self.all.count
-  end
-
-
   module InstanceMethods
-      def initialize
+    def initialize
       self.class.all << self
     end
   end
